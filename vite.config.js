1a284474bd225b40b09b492e19c9f21d46ac5544
Clément Denis <clsined@gmail.com>
2026-05-24 17:40:13 +0200

    fix vite config for pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/', // remplacer par '/' si DNS
+  base: '/site-magnetisme/', // remplacer par '/' si DNS
   root: 'src',
   publicDir: '../assets',
   build: {
