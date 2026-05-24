4960ba50c28b67ac43d6a624322bda88a48647a9
Clement Denis <clement.denis_ext@michelin.com>
2026-05-24 09:36:55 +0200

    pre v1

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  root: '.',
-  publicDir: 'assets',
+  root: 'src',
+  publicDir: '../assets',
   build: {
-    outDir: 'dist',
+    outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        visualisation: resolve(__dirname, 'src/visualisation-arbre.html'),
+      },
+    },
   },
   server: {
     open: true,
